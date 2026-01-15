a81e7f39a5da4696244f12d8a0227e74910f5d15
mssnkmryrk-a11y <mss.nkmr.yrk@gmail.com>
2026-01-15 20:54:39 +0900

    Create main.tsx

---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import WorkApp from "./index";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <WorkApp />
+  </React.StrictMode>
+);
